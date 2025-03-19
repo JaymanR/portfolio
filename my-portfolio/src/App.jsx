94e4f25042e8b9788078d0a3f01400fdca7560ed
Jayman Rana <jayman.code@gmail.com>
2025-03-19 14:20:04 -0400

    setup Contact section

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -6,6 +6,7 @@ import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
+import { Contact } from "./components/sections/Contact";
 
 import "./App.css";
 import "./index.css";
@@ -46,6 +47,7 @@ function App() {
         <Home />
         <About />
         <Projects />
+        <Contact />
       </div>
     </>
   );
